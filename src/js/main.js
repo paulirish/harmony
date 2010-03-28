42927b8cef8561c7e2eb0d6a2601e9039b02e378
Paul Irish <paul.irish@gmail.com>
2010-03-28 15:51:16 -0400

    everyones favorite closure pattern. and smushing all the code into a single file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,372 @@
-var i, brush, BRUSHES = ["ribbon"],
-COLOR = [0, 0, 0], BACKGROUND_COLOR = [250, 250, 250],
-SCREEN_WIDTH = window.innerWidth,
-SCREEN_HEIGHT = window.innerHeight,
-container, foregroundColorSelector, backgroundColorSelector, menu, about,
-canvas, flattenCanvas, context,
-isForegroundColorSelectorVisible = false, isBackgroundColorSelectorVisible = false, isAboutVisible = false,
-isMenuMouseOver = false, shiftKeyIsDown = false, altKeyIsDown = false;
 
-init();
+
+(function(window,document,undefined){
+
+
+
+
+
+
+    function Palette()
+    {
+    	var canvas, context, offsetx, offsety, radius = 90,
+    	count = 1080, oneDivCount = 1 / count, countDiv360 = count / 360, degreesToRadians = Math.PI / 180,
+    	i, color, angle, angle_cos, angle_sin, gradient;
+
+    	canvas = document.createElement("canvas");
+    	canvas.width = 250;
+    	canvas.height = 250;
+
+    	offsetx = canvas.width / 2;
+    	offsety = canvas.height / 2;
+
+    	context = canvas.getContext("2d");
+    	context.lineWidth = 1;
+
+    	function HSB2RGB(hue, sat, val)
+    	{
+    		var red, green, blue,
+    		i, f, p, q, t;
+
+    		if (val == 0)
+    			return [ 0, 0, 0 ];
+
+    		hue *= 0.016666667; // /= 60;
+    		sat *= 0.01; // /= 100;
+    		val *= 0.01; // /= 100;
+
+    		i = Math.floor(hue);
+    		f = hue - i;
+    		p = val * (1 - sat);
+    		q = val * (1 - (sat * f));
+    		t = val * (1 - (sat * (1 - f)));
+
+    		switch(i)
+    		{
+    			case 0: red = val; green = t; blue = p; break;
+    			case 1: red = q; green = val; blue = p; break;
+    			case 2: red = p; green = val; blue = t; break;
+    			case 3: red = p; green = q; blue = val; break;
+    			case 4: red = t; green = p; blue = val; break;
+    			case 5: red = val; green = p; blue = q; break;
+    		}
+
+    		return [red, green, blue];
+    	}
+
+    	// http://www.boostworthy.com/blog/?p=226	
+
+    	for(i = 0; i < count; i++)
+    	{
+    		color = HSB2RGB( Math.floor( (i * oneDivCount) * 360 ), 100, 100);
+    		angle = i / countDiv360 * degreesToRadians;
+    		angle_cos = Math.cos(angle);
+    		angle_sin = Math.sin(angle);
+
+    		context.strokeStyle = "rgb(" + Math.floor( color[0] * 255 ) + "," + Math.floor( color[1] * 255 ) + "," + Math.floor( color[2] * 255 ) + ")";
+    		context.beginPath();
+    		context.moveTo(angle_cos + offsetx, angle_sin + offsety);
+    		context.lineTo(angle_cos * radius + offsetx, angle_sin * radius + offsety);
+    		context.stroke();
+    	}
+
+    	gradient = context.createRadialGradient(offsetx, offsetx, 0, offsetx, offsetx, radius);
+    	gradient.addColorStop(0.1, 'rgba(255, 255, 255, 1)');
+    	gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
+
+    	context.fillStyle = gradient;
+    	context.fillRect(0, 0, canvas.width, canvas.height);
+
+    	return canvas;
+    }
+
+
+
+
+    function ColorSelector( gradient )
+    {
+    	this.init( gradient );
+    }
+
+    ColorSelector.prototype =
+    {
+    	container: null,
+
+    	hue: null,
+    	hueSelector: null,
+    	hueData: null,
+
+    	luminosity: null,
+    	luminositySelector: null,
+    	luminosityData: null,
+    	luminosityPosition: null,
+
+    	init: function(gradient)
+    	{
+    		var context;
+
+    		this.container = document.createElement("div");
+    		this.container.style.position = 'absolute';
+    		this.container.style.width = '250px';
+    		this.container.style.height = '250px';
+    		this.container.style.visibility = 'hidden';
+    		this.container.style.cursor = 'pointer';
+
+    		this.hue = document.createElement("canvas");
+    		this.hue.width = gradient.width;
+    		this.hue.height = gradient.height;
+
+    		context = this.hue.getContext("2d");
+    		context.drawImage(gradient, 0, 0, this.hue.width, this.hue.height);
+
+    		this.hueData = context.getImageData(0, 0, this.hue.width, this.hue.height).data;	
+
+    		this.container.appendChild(this.hue);
+
+    		this.luminosity = document.createElement("canvas");
+    		this.luminosity.style.position = 'absolute';
+    		this.luminosity.style.left = '0px';
+    		this.luminosity.style.top = '0px';
+    		this.luminosity.width = 250;
+    		this.luminosity.height = 250;
+
+    		this.container.appendChild(this.luminosity);
+
+    		this.updateLuminosity( [255, 255, 255] );
+
+    		this.hueSelector = document.createElement("canvas");
+    		this.hueSelector.style.position = 'absolute';
+    		this.hueSelector.style.left = ((this.hue.width - 15) / 2 ) + 'px';
+    		this.hueSelector.style.top = ((this.hue.height - 15) / 2 ) + 'px';
+    		this.hueSelector.width = 15;
+    		this.hueSelector.height = 15;
+
+    		context = this.hueSelector.getContext("2d");
+    		context.lineWidth = 2;
+    		context.strokeStyle = "rgba(0, 0, 0, 0.5)";
+    		context.beginPath();
+    		context.arc(8, 8, 6, 0, Math.PI * 2, true);
+    		context.stroke();
+    		context.strokeStyle = "rgba(256, 256, 256, 0.8)";
+    		context.beginPath();
+    		context.arc(7, 7, 6, 0, Math.PI * 2, true);
+    		context.stroke();
+
+    		this.container.appendChild( this.hueSelector );
+
+    		this.luminosityPosition = [ (gradient.width - 15), (gradient.height - 15) / 2 ];
+
+    		this.luminositySelector = document.createElement("canvas");
+    		this.luminositySelector.style.position = 'absolute';
+    		this.luminositySelector.style.left = (this.luminosityPosition[0] - 7) + 'px';
+    		this.luminositySelector.style.top = (this.luminosityPosition[1] - 7) + 'px';
+    		this.luminositySelector.width = 15;
+    		this.luminositySelector.height = 15;
+
+    		context = this.luminositySelector.getContext("2d");
+    		context.drawImage(this.hueSelector, 0, 0, this.luminositySelector.width, this.luminositySelector.height);
+
+    		this.container.appendChild(this.luminositySelector);
+    	},
+
+    	show: function()
+    	{
+    		this.container.style.visibility = 'visible';
+    	},
+
+    	hide: function()
+    	{
+    		this.container.style.visibility = 'hidden';		
+    	},
+
+    	updateLuminosity: function( color )
+    	{
+    		var context, angle, angle_cos, angle_sin, shade, offsetx, offsety,
+    		inner_radius = 100, outter_radius = 120, i, count = 1080 / 2, oneDivCount = 1 / count, degreesToRadians = Math.PI / 180,
+    		countDiv360 = (count / 360);
+
+    		offsetx = this.luminosity.width / 2;
+    		offsety = this.luminosity.height / 2;
+
+    		context = this.luminosity.getContext("2d");
+    		context.lineWidth = 3;
+    		context.clearRect(0, 0, this.luminosity.width, this.luminosity.height);
+
+    		for(i = 0; i < count; i++)
+    		{
+    			angle = i / countDiv360 * degreesToRadians;
+    			angle_cos = Math.cos(angle);
+    			angle_sin = Math.sin(angle);
+
+    			shade = 255 - (i * oneDivCount /* / count */) * 255;
+
+    			context.strokeStyle = "rgb(" + Math.floor( color[0] - shade ) + "," + Math.floor( color[1] - shade ) + "," + Math.floor( color[2] - shade ) + ")";
+    			context.beginPath();
+    			context.moveTo(angle_cos * inner_radius + offsetx, angle_sin * inner_radius + offsety);
+    			context.lineTo(angle_cos * outter_radius + offsetx, angle_sin * outter_radius + offsety);
+    			context.stroke();
+    		}
+
+    		this.luminosityData = context.getImageData(0, 0, this.luminosity.width, this.luminosity.height).data;
+    	},
+
+    	update: function(x, y)
+    	{
+    		var dx, dy, d, nx, ny;
+
+    		dx = x - 125;
+    		dy = y - 125;
+    		d = Math.sqrt( dx * dx + dy * dy );
+
+    		if (d < 90)
+    		{
+    			this.hueSelector.style.left = (x - 7) + 'px';
+    			this.hueSelector.style.top = (y - 7) + 'px';
+    			this.updateLuminosity( [ this.hueData[(x + (y * 250)) * 4], this.hueData[(x + (y * 250)) * 4 + 1], this.hueData[(x + (y * 250)) * 4 + 2] ] );
+    		}
+    		else if (d > 100)
+    		{
+    			nx = dx / d;
+    			ny = dy / d;
+
+    			this.luminosityPosition[0] = (nx * 110) + 125;
+    			this.luminosityPosition[1] = (ny * 110) + 125;
+
+    			this.luminositySelector.style.left = ( this.luminosityPosition[0] - 7) + 'px';
+    			this.luminositySelector.style.top = ( this.luminosityPosition[1] - 7) + 'px';			
+    		}
+    	},
+
+    	getColor: function()
+    	{
+    		var x, y;
+
+    		x = Math.floor(this.luminosityPosition[0]);
+    		y = Math.floor(this.luminosityPosition[1]);
+
+    		return [ this.luminosityData[(x + (y * 250)) * 4], this.luminosityData[(x + (y * 250)) * 4 + 1], this.luminosityData[(x + (y * 250)) * 4 + 2] ];
+    	}
+    }
+
+
+
+
+    function ribbon( context )
+    {
+    	this.init( context );
+    }
+
+    ribbon.prototype =
+    {
+    	context: null,
+
+    	mouseX: null, mouseY: null,
+
+    	painters: null,
+
+    	interval: null,
+
+    	init: function( context )
+    	{
+    		this.context = context;
+    		this.context.lineWidth = 1;
+    		this.context.globalCompositeOperation = 'source-over';
+
+    		this.mouseX = SCREEN_WIDTH / 2;
+    		this.mouseY = SCREEN_HEIGHT / 2;
+
+    		this.painters = new Array();
+
+    		for (var i = 0; i < 50; i++)
+    		{
+    			this.painters.push({ dx: SCREEN_WIDTH / 2, dy: SCREEN_HEIGHT / 2, ax: 0, ay: 0, div: 0.1, ease: Math.random() * 0.2 + 0.6 });
+    		}
+
+    		this.isDrawing = false;
+
+    		this.interval = setInterval( bargs( function( _this ) { _this.update(); return false; }, this ), 1000/60 );
+    	},
+
+    	destroy: function()
+    	{
+    		clearInterval(this.interval);
+    	},
+
+    	strokeStart: function( mouseX, mouseY )
+    	{
+    		this.mouseX = mouseX;
+    		this.mouseY = mouseY
+
+    		this.context.strokeStyle = "rgba(" + COLOR[0] + ", " + COLOR[1] + ", " + COLOR[2] + ", 0.05 )";		
+
+    		for (var i = 0; i < this.painters.length; i++)
+    		{
+    			this.painters[i].dx = mouseX;
+    			this.painters[i].dy = mouseY;
+    		}
+
+    		this.shouldDraw = true;
+    	},
+
+    	stroke: function( mouseX, mouseY )
+    	{
+    		this.mouseX = mouseX;
+    		this.mouseY = mouseY;
+    	},
+
+    	strokeEnd: function()
+    	{
+
+    	},
+
+    	update: function()
+    	{
+    		var i;
+
+    		for (i = 0; i < this.painters.length; i++)
+    		{
+    			this.context.beginPath();
+    			this.context.moveTo(this.painters[i].dx, this.painters[i].dy);		
+
+    			this.painters[i].dx -= this.painters[i].ax = (this.painters[i].ax + (this.painters[i].dx - this.mouseX) * this.painters[i].div) * this.painters[i].ease;
+    			this.painters[i].dy -= this.painters[i].ay = (this.painters[i].ay + (this.painters[i].dy - this.mouseY) * this.painters[i].div) * this.painters[i].ease;
+    			this.context.lineTo(this.painters[i].dx, this.painters[i].dy);
+    			this.context.stroke();
+    		}
+    	}
+    }
+
+    function bargs( _fn )
+    {
+    	var n, args = [];
+    	for( n = 1; n < arguments.length; n++ )
+    		args.push( arguments[ n ] );
+    	return function () { return _fn.apply( this, args ); };
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var i, brush, BRUSHES = ["ribbon"],
+    COLOR = [0, 0, 0], BACKGROUND_COLOR = [250, 250, 250],
+    SCREEN_WIDTH = window.innerWidth,
+    SCREEN_HEIGHT = window.innerHeight,
+    container, foregroundColorSelector, backgroundColorSelector, menu, about,
+    canvas, flattenCanvas, context,
+    isForegroundColorSelectorVisible = false, isBackgroundColorSelectorVisible = false, isAboutVisible = false,
+    isMenuMouseOver = false, shiftKeyIsDown = false, altKeyIsDown = false;
+
+    init();
+
 
 function init()
 {
@@ -423,3 +782,12 @@ function cleanPopUps()
 		isAboutVisible = false;
 	}
 }
+
+
+
+
+
+
+
+
+})(this,this.document);
